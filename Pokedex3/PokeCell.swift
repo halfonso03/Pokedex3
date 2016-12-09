f136a17b8fa35f77d9b599b0349a221320bc7875
halfonso03 <hector.alfonso@yahoo.com>
2016-12-09 18:33:30 -0500

    second commit

---
+++ Pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Alfonso, Hector I. on 12/8/16.
+//  Copyright © 2016 Alfonso, Hector I. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ Pokedex3/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Alfonso, Hector I. on 12/7/16.
+//  Copyright © 2016 Alfonso, Hector I. All rights reserved.
+//
+
+import Foundation
