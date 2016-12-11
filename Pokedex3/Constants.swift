4ae6aaed8f52ae1019371853df3657ddb893f4ba
halfonso03 <hector.alfonso@yahoo.com>
2016-12-11 15:18:32 -0500

    aded details

---
+++ Pokedex3/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Pokedex3
+//
+//  Created by Alfonso, Hector I. on 12/10/16.
+//  Copyright © 2016 Alfonso, Hector I. All rights reserved.
+//
+
+import Foundation
---
+++ Pokedex3/PokemonDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokedex3
+//
+//  Created by Alfonso, Hector I. on 12/9/16.
+//  Copyright © 2016 Alfonso, Hector I. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
